f4b164ee6e7234c35fdcd02cec1730e456c3ead8
occamskatana <johngallweycarter@gmail.com>
2015-12-20 23:28:28 -0500

    bugfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :define_user
-
-  def define_user
-  	@user = current_user
-  end
+  
 end
